05c2709d7b8a0cc437423eebb8906becfdf49eef
yuwenjian <740225978@qq.com>
2023-12-13 06:31:54 +0000

    同步 SDK 版本

--- sdkVersions.ts
+++ sdkVersions.ts
@@ -1,9 +1,9 @@
 const sdkVersions = {
   taptap: {
-    unity: "111111",
-    android: "222222",
-    ios: "3333333",
-    unreal: "44444444",
+    unity: "3.26.5",
+    android: "3.24.1",
+    ios: "3.23.1",
+    unreal: "3.25.1",
     rtc: "1.1.0",
     adr: "1.1.5"
   },
@@ -22,8 +22,8 @@ const sdkVersions = {
     }
   },
   tapadn: {
-    unity: "55555555",
-    android: "66666666",
+    unity: "3.26.5",
+    android: "3.24.1",
   }
 };
 
