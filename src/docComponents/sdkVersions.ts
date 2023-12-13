208f145a10708bfaaec4c9f9bfb80d0f0a6f482e
yuwenjian <740225978@qq.com>
2023-12-13 18:12:53 +0800

    dfg

--- src/docComponents/sdkVersions.ts
+++ src/docComponents/sdkVersions.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD:src/docComponents/sdkVersions.ts
 const taptapUnity = "11111"
 const taptapIos = "22222"
 const taptapAndroid = "22222"
 const taptapUnreal = "222222"
-=======
-const taptapUnity = "3.26.5"
-const taptapIos = "3.23.1"
-const taptapAndroid = "3.24.1"
-const taptapUnreal = "3.25.1"
->>>>>>> d40192da5c8fbe5e866055c36ad68c6e74d63cb7:sdkVersions.ts
 
 const sdkVersions = {
   taptap: {
